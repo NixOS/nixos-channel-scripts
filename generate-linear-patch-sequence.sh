d73ddef07c8d5f5048883da39c525e8b84936794
Eelco Dolstra <eelco.dolstra@logicblox.com>
2012-04-06 20:17:13 +0000

    git-svn-id: https://nixos.org/repos/nix/release/trunk/channels@33658 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

--- generate-linear-patch-sequence.sh
+++ generate-linear-patch-sequence.sh
@@ -6,11 +6,11 @@ prev=""
 
 for i in $(cd "$releasesDir" && ls -d nixpkgs-*pre* | sort -n); do
     if test -e "$releasesDir/$i/MANIFEST"; then
-	if test -n "$prev" -a ! -e "$releasesDir/$i/patches-created"; then
+	if test -n "$prev" -a ! -e "$releasesDir/$i/.patches-created"; then
 	    echo $prev "->" $i
 	    date
 	    time ./generate-patches.sh "$releasesDir/$prev" "$releasesDir/$i"
-	    touch "$releasesDir/$i/patches-created"
+	    touch "$releasesDir/$i/.patches-created"
 	fi
 	prev=$i
     fi
