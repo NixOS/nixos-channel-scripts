4d2e345db0e8f8bbe616b67aef5890858eced1da
Eelco Dolstra <eelco.dolstra@logicblox.com>
2012-06-08 21:46:29 +0000

    * Generate patches between NixOS releases.
    git-svn-id: https://nixos.org/repos/nix/release/trunk/channels@34393 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

--- generate-linear-patch-sequence.sh
+++ generate-linear-patch-sequence.sh
@@ -15,3 +15,19 @@ for i in $(cd "$releasesDir" && ls -d nixpkgs-*pre* | sort -n); do
 	prev=$i
     fi
 done
+
+releasesDir=/data/releases/nixos
+
+prev=""
+
+for i in $(cd "$releasesDir" && ls -d nixos-*pre* | sort -n); do
+    if test -e "$releasesDir/$i/MANIFEST"; then
+	if test -n "$prev" -a ! -e "$releasesDir/$i/.patches-created"; then
+	    echo $prev "->" $i
+	    date
+	    time ./generate-patches.sh "$releasesDir/$prev" "$releasesDir/$i"
+	    touch "$releasesDir/$i/.patches-created"
+	fi
+	prev=$i
+    fi
+done
