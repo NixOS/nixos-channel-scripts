0a96d82417fc78c9a75b8274ad7d1f524c29df4d
Eelco Dolstra <eelco.dolstra@logicblox.com>
2013-10-09 09:02:09 +0000

    git-svn-id: https://nixos.org/repos/nix/release/trunk/channels@34773 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

--- delete-old-releases.sh
+++ delete-old-releases.sh
@@ -12,7 +12,7 @@ find /data/releases/nixos/ /data/releases/nixpkgs/ -maxdepth 1 -name ".tmp*" -mt
 done
 
 # Remove old NixOS releases. 
-find /data/releases/nixos/ -maxdepth 1 -name "nixos-*pre*" -mtime +20 | sort | while read rel; do 
+find /data/releases/nixos/ -maxdepth 1 -name "nixos-*pre*" -mtime +14 | sort | while read rel; do 
     if [ -e $rel/keep ]; then 
 	echo "keeping NixOS release $rel" >&2
 	continue
@@ -30,6 +30,8 @@ find /data/releases/nixpkgs/ -maxdepth 1 -name "nixpkgs-*pre*" -mtime +30 | sort
     echo "removing old Nixpkgs release $rel" >&2
     mv $rel $trash/
 done
+
+exit 0
 
 # Remove unreferenced NARs/patches (but only if they're older than 2
 # weeks, to prevent messing with binary patch generation in progress).
