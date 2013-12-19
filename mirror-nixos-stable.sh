9dd6c6cf9231b508a9d6498521403a2ec4838318
Eelco Dolstra <eelco.dolstra@logicblox.com>
2013-12-19 15:38:27 +0000

    * Use releases.nixos.org.
    git-svn-id: https://nixos.org/repos/nix/release/trunk/channels@34805 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

--- mirror-nixos-stable.sh
+++ mirror-nixos-stable.sh
@@ -9,20 +9,20 @@ release=$(basename $releaseDir)
 # Generate a .htaccess with some symbolic redirects to the latest ISOs.
 htaccess=/data/releases/nixos/.htaccess
 
-baseURL="http://nixos.org/releases/nixos/$version/$release"
-echo "Redirect /releases/nixos/latest $baseURL" > $htaccess.tmp
+baseURL="http://releases.nixos.org/nixos/$version/$release"
+echo "Redirect /nixos/latest $baseURL" > $htaccess.tmp
 fn=$(cd $releaseDir && echo nixos-minimal-*-i686-linux.iso)
-echo "Redirect /releases/nixos/latest-iso-minimal-i686-linux $baseURL/$fn" >> $htaccess.tmp
+echo "Redirect /nixos/latest-iso-minimal-i686-linux $baseURL/$fn" >> $htaccess.tmp
 fn=$(cd $releaseDir && echo nixos-minimal-*-x86_64-linux.iso)
-echo "Redirect /releases/nixos/latest-iso-minimal-x86_64-linux $baseURL/$fn" >> $htaccess.tmp
+echo "Redirect /nixos/latest-iso-minimal-x86_64-linux $baseURL/$fn" >> $htaccess.tmp
 fn=$(cd $releaseDir && echo nixos-graphical-*-i686-linux.iso)
-echo "Redirect /releases/nixos/latest-iso-graphical-i686-linux $baseURL/$fn" >> $htaccess.tmp
+echo "Redirect /nixos/latest-iso-graphical-i686-linux $baseURL/$fn" >> $htaccess.tmp
 fn=$(cd $releaseDir && echo nixos-graphical-*-x86_64-linux.iso)
-echo "Redirect /releases/nixos/latest-iso-graphical-x86_64-linux $baseURL/$fn" >> $htaccess.tmp
+echo "Redirect /nixos/latest-iso-graphical-x86_64-linux $baseURL/$fn" >> $htaccess.tmp
 fn=$(cd $releaseDir && echo nixos-*-i686-linux.ova)
-echo "Redirect /releases/nixos/latest-ova-i686-linux $baseURL/$fn" >> $htaccess.tmp
+echo "Redirect /nixos/latest-ova-i686-linux $baseURL/$fn" >> $htaccess.tmp
 fn=$(cd $releaseDir && echo nixos-*-x86_64-linux.ova)
-echo "Redirect /releases/nixos/latest-ova-x86_64-linux $baseURL/$fn" >> $htaccess.tmp
+echo "Redirect /nixos/latest-ova-x86_64-linux $baseURL/$fn" >> $htaccess.tmp
 
 mv $htaccess.tmp $htaccess
 
