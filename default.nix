76ca1da3ee06aa6adca92d67ae717228ff9fb2ed
Eelco Dolstra <eelco.dolstra@logicblox.com>
2016-08-22 17:18:44 +0200

    Add $out/bin to $PATH

--- default.nix
+++ default.nix
@@ -24,7 +24,7 @@ stdenv.mkDerivation {
       -I ${nix}/include/nix -lnixmain -lnixexpr -lnixformat -lsqlite3 -lgc
 
     cp ${./mirror-nixos-branch.pl} $out/bin/mirror-nixos-branch
-    wrapProgram $out/bin/mirror-nixos-branch --set PERL5LIB $PERL5LIB --prefix PATH : ${wget}/bin:${git}/bin:${nix}/bin:${gnutar}/bin:${xz}/bin
+    wrapProgram $out/bin/mirror-nixos-branch --set PERL5LIB $PERL5LIB --prefix PATH : ${wget}/bin:${git}/bin:${nix}/bin:${gnutar}/bin:${xz}/bin:$out/bin
   '';
 
 }
