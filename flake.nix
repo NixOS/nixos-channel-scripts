ffeb2b629e9c8f66c582133f3eaa0582fc3357b0
John Ericson <John.Ericson@Obsidian.Systems>
2025-04-09 00:37:24 -0400

    Format all Nix files

--- flake.nix
+++ flake.nix
@@ -3,39 +3,46 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05-small";
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     {
       overlays.default = final: prev: {
-        nixos-channel-native-programs = with final; stdenv.mkDerivation {
-          name = "nixos-channel-native-programs";
-          buildInputs = [
+        nixos-channel-native-programs =
+          with final;
+          stdenv.mkDerivation {
+            name = "nixos-channel-native-programs";
+            buildInputs = [
               nix
               pkg-config
               boehmgc
               nlohmann_json
               boost
               sqlite
-          ];
+            ];
 
-          buildCommand = let
-            nixHasSignalsHh = nixpkgs.lib.strings.versionAtLeast nix.version "2.19";
-          in ''
-            mkdir -p $out/bin
+            buildCommand =
+              let
+                nixHasSignalsHh = nixpkgs.lib.strings.versionAtLeast nix.version "2.19";
+              in
+              ''
+                mkdir -p $out/bin
 
-            g++ -Os -g ${./index-debuginfo.cc} -Wall -std=c++14 -o $out/bin/index-debuginfo -I . \
-              $(pkg-config --cflags nix-main) \
-              $(pkg-config --libs nix-main) \
-              $(pkg-config --libs nix-store) \
-              -lsqlite3 \
-              ${nixpkgs.lib.optionalString nixHasSignalsHh "-DHAS_SIGNALS_HH"}
-          '';
-        };
+                g++ -Os -g ${./index-debuginfo.cc} -Wall -std=c++14 -o $out/bin/index-debuginfo -I . \
+                  $(pkg-config --cflags nix-main) \
+                  $(pkg-config --libs nix-main) \
+                  $(pkg-config --libs nix-store) \
+                  -lsqlite3 \
+                  ${nixpkgs.lib.optionalString nixHasSignalsHh "-DHAS_SIGNALS_HH"}
+              '';
+          };
 
-        nixos-channel-scripts = with final; stdenv.mkDerivation {
-          name = "nixos-channel-scripts";
+        nixos-channel-scripts =
+          with final;
+          stdenv.mkDerivation {
+            name = "nixos-channel-scripts";
 
-          buildInputs = with perlPackages;
-            [ nix
+            buildInputs = with perlPackages; [
+              nix
               sqlite
               makeWrapper
               perl
@@ -51,24 +58,37 @@
               nix-index
             ];
 
-          buildCommand = ''
-            mkdir -p $out/bin
+            buildCommand = ''
+              mkdir -p $out/bin
 
-            cp ${./mirror-nixos-branch.pl} $out/bin/mirror-nixos-branch
-            wrapProgram $out/bin/mirror-nixos-branch \
-              --set PERL5LIB $PERL5LIB \
-              --set XZ_OPT "-T0" \
-              --prefix PATH : ${lib.makeBinPath [ wget git nix gnutar xz rsync openssh nix-index nixos-channel-native-programs ]}
+              cp ${./mirror-nixos-branch.pl} $out/bin/mirror-nixos-branch
+              wrapProgram $out/bin/mirror-nixos-branch \
+                --set PERL5LIB $PERL5LIB \
+                --set XZ_OPT "-T0" \
+                --prefix PATH : ${
+                  lib.makeBinPath [
+                    wget
+                    git
+                    nix
+                    gnutar
+                    xz
+                    rsync
+                    openssh
+                    nix-index
+                    nixos-channel-native-programs
+                  ]
+                }
 
-            patchShebangs $out/bin
-          '';
-        };
+              patchShebangs $out/bin
+            '';
+          };
 
       };
 
-      defaultPackage.x86_64-linux = (import nixpkgs {
-        system = "x86_64-linux";
-        overlays = [ self.overlays.default ];
-      }).nixos-channel-scripts;
+      defaultPackage.x86_64-linux =
+        (import nixpkgs {
+          system = "x86_64-linux";
+          overlays = [ self.overlays.default ];
+        }).nixos-channel-scripts;
     };
 }
