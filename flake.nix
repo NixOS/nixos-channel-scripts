f373184bedef93f7b3d29b2122d2de103090fcca
John Ericson <John.Ericson@Obsidian.Systems>
2025-04-09 00:55:26 -0400

    Fix build with Nix 2.28, improve packaging in other ways

--- flake.nix
+++ flake.nix
@@ -8,66 +8,65 @@
     { self, nixpkgs }:
     {
       overlays.default = final: prev: {
-        nixos-channel-native-programs =
-          with final;
-          stdenv.mkDerivation {
-            name = "nixos-channel-native-programs";
-            buildInputs = [
-              nix
-              pkg-config
-              boehmgc
-              nlohmann_json
-              boost
-              sqlite
-            ];
+        nixos-channel-native-programs = final.stdenv.mkDerivation {
+          name = "nixos-channel-native-programs";
 
-            buildCommand =
-              let
-                nixHasSignalsHh = nixpkgs.lib.strings.versionAtLeast nix.version "2.19";
-              in
-              ''
-                mkdir -p $out/bin
+          strictDeps = true;
 
-                g++ -Os -g ${./index-debuginfo.cc} -Wall -std=c++14 -o $out/bin/index-debuginfo -I . \
-                  $(pkg-config --cflags nix-main) \
-                  $(pkg-config --libs nix-main) \
-                  $(pkg-config --libs nix-store) \
-                  -lsqlite3 \
-                  ${nixpkgs.lib.optionalString nixHasSignalsHh "-DHAS_SIGNALS_HH"}
-              '';
-          };
+          nativeBuildInputs = with final.buildPackages; [
+            pkg-config
+          ];
 
-        nixos-channel-scripts =
-          with final;
-          stdenv.mkDerivation {
-            name = "nixos-channel-scripts";
+          buildInputs = with final; [
+            nix
+            nlohmann_json
+            boost
+          ];
 
-            buildInputs = with perlPackages; [
-              nix
-              sqlite
-              makeWrapper
-              perl
-              FileSlurp
-              LWP
-              LWPProtocolHttps
-              ListMoreUtils
-              DBDSQLite
-              NetAmazonS3
-              brotli
-              jq
-              nixos-channel-native-programs
-              nix-index
-            ];
+          buildCommand = ''
+            mkdir -p $out/bin
 
-            buildCommand = ''
-              mkdir -p $out/bin
+            $CXX \
+              -Os -g -Wall \
+              -std=c++14 \
+              $(pkg-config --libs --cflags nix-store) \
+              $(pkg-config --libs --cflags nix-main) \
+              -I . \
+              ${./index-debuginfo.cc} \
+              -o $out/bin/index-debuginfo
+          '';
+        };
 
-              cp ${./mirror-nixos-branch.pl} $out/bin/mirror-nixos-branch
-              wrapProgram $out/bin/mirror-nixos-branch \
-                --set PERL5LIB $PERL5LIB \
-                --set XZ_OPT "-T0" \
-                --prefix PATH : ${
-                  lib.makeBinPath [
+        nixos-channel-scripts = final.stdenv.mkDerivation {
+          name = "nixos-channel-scripts";
+
+          strictDeps = true;
+
+          nativeBuildInputs = with final.buildPackages; [
+            makeWrapper
+          ];
+
+          buildInputs = with final.perlPackages; [
+            final.perl
+            FileSlurp
+            LWP
+            LWPProtocolHttps
+            ListMoreUtils
+            DBDSQLite
+            NetAmazonS3
+          ];
+
+          buildCommand = ''
+            mkdir -p $out/bin
+
+            cp ${./mirror-nixos-branch.pl} $out/bin/mirror-nixos-branch
+            wrapProgram $out/bin/mirror-nixos-branch \
+              --set PERL5LIB $PERL5LIB \
+              --set XZ_OPT "-T0" \
+              --prefix PATH : ${
+                final.lib.makeBinPath (
+                  with final;
+                  [
                     wget
                     git
                     nix
@@ -78,15 +77,16 @@
                     nix-index
                     nixos-channel-native-programs
                   ]
-                }
+                )
+              }
 
-              patchShebangs $out/bin
-            '';
-          };
+            patchShebangs $out/bin
+          '';
+        };
 
       };
 
-      defaultPackage.x86_64-linux =
+      packages.x86_64-linux.default =
         (import nixpkgs {
           system = "x86_64-linux";
           overlays = [ self.overlays.default ];
